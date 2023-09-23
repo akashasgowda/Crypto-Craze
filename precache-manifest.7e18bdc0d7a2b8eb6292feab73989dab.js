2426be2eab761b4b5a715550a339217aaadaf28f
Valkeri 47 <akashasgowda14>
2023-09-23 12:57:54 +0530

    Updates

---
+++ precache-manifest.7e18bdc0d7a2b8eb6292feab73989dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69171ff322a25664e74d",
+    "url": "/Crypto-Craze/static/css/main.b582c594.chunk.css"
+  },
+  {
+    "revision": "69171ff322a25664e74d",
+    "url": "/Crypto-Craze/static/js/main.69171ff3.chunk.js"
+  },
+  {
+    "revision": "919ca2c118c90095f884",
+    "url": "/Crypto-Craze/static/js/1.919ca2c1.chunk.js"
+  },
+  {
+    "revision": "f89e687153687e307daf",
+    "url": "/Crypto-Craze/static/js/runtime~main.f89e6871.js"
+  },
+  {
+    "revision": "f0803f195d7da2a85a940cc40190bb2b",
+    "url": "/Crypto-Craze/static/media/cc_logo.f0803f19.png"
+  },
+  {
+    "revision": "02ff97004c0d4efc983dd4a095feb642",
+    "url": "/Crypto-Craze/index.html"
+  }
+];
